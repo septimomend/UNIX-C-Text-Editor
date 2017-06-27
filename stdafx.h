dbd1ae31e190b16e896f10886737ca77e537c551
septimomend <chapkailo.ivan@gmail.com>
2017-06-27 18:12:53 +0300

    add precompiled header

---
+++ stdafx.h
@@ -0,0 +1,25 @@
+// precompiled header
+/*
+ *
+ * Author: septimomend (Ivan Chapkailo)
+ *
+ * 27.06.2017
+ *
+ */
+
+ #define _DEFAULT_SOURCE
+ #define _BSD_SOURCE
+ #define _GNU_SOURCE
+
+ #include <ctype.h>
+ #include <errno.h>
+ #include <fcntl.h>
+ #include <stdio.h>
+ #include <stdarg.h>
+ #include <stdlib.h>
+ #include <string.h>
+ #include <sys/ioctl.h>
+ #include <sys/types.h>
+ #include <termios.h>
+ #include <time.h>
+ #include <unistd.h>
