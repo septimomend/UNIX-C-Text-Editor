9490634df91ac57030048180c320068ccd7c70bb
septimomend <chapkailo.ivan@gmail.com>
2017-06-29 16:30:07 +0300

    file extensions and C/C++ keywords

---
+++ FileTypes.h
@@ -0,0 +1,17 @@
+// FileTypes.h -- header file
+/*
+ *
+ * Author: septimomend (Ivan Chapkailo)
+ *
+ * 29.06.2017
+ *
+ */
+
+#include "stdafx.h"
+
+char *pFileExtension[] = { ".c", ".h", ".cpp", NULL };
+char *pKeywords[] = { "switch", "if", "while", "for", "break", "continue", "return", "else",
+                      "struct", "union", "typedef", "static", "enum", "class", "case",
+                      "int|", "long|", "double|", "float|", "char|", "unsigned|", "signed|",
+                      "void|", NULL
+                    };
