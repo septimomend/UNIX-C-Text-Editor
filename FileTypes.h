1bf0c727ef0f8add34bcd74542b9725dfcb0f5b4
septimomend <chapkailo.ivan@gmail.com>
2017-06-29 16:41:53 +0300

    added new keywords -> TODO: add more keywords

--- FileTypes.h
+++ FileTypes.h
@@ -9,9 +9,11 @@
 
 #include "stdafx.h"
 
+// TODO: add more keywords
 char *pFileExtension[] = { ".c", ".h", ".cpp", NULL };
 char *pKeywords[] = { "switch", "if", "while", "for", "break", "continue", "return", "else",
                       "struct", "union", "typedef", "static", "enum", "class", "case",
+
                       "int|", "long|", "double|", "float|", "char|", "unsigned|", "signed|",
-                      "void|", NULL
+                      "void|", "bool", NULL
                     };
