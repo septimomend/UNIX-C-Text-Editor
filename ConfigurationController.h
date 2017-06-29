5d58278ae4ea7a1bce27b8ebe22cda0fd6ba6471
septimomend <chapkailo.ivan@gmail.com>
2017-06-29 17:06:56 +0300

    // TODO: declare ConfigurationController to full

---
+++ ConfigurationController.h
@@ -0,0 +1,16 @@
+// ConfigurationController.h -- header file
+/*
+ *
+ * Author: septimomend (Ivan Chapkailo)
+ *
+ * 29.06.2017
+ *
+ */
+
+#include "stdafx.h"
+#include "SyntaxController.h"
+
+class ConfigurationController : public SyntaxController
+{
+  // TODO: add vars declaration
+}
