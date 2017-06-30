153828a52299eb7674e7c9ffaddefc7cdac2bfd6
septimomend <chapkailo.ivan@gmail.com>
2017-06-30 11:33:18 +0300

    class declaration

--- ConfigurationController.h
+++ ConfigurationController.h
@@ -12,5 +12,18 @@
 
 class ConfigurationController : public SyntaxController
 {
-  // TODO: add vars declaration
+  int configX, configY;
+  int rowX;
+  int disableRow; // disable
+  int disableClr; // row and colour
+  int enableRow; // and enable row
+  int enableClr; // and colour
+  int rowCount; // number of rows
+  int smear;
+  char *pFilename;
+  char statusMessage[80];
+  time_t statusMessageTime;
+  RowController *pRowObj; // RowController's object
+  SyntaxController *pSyntaxObj; // SyntaxController's object
+  struct termios baseTermiosObj; // for working with terminal's i/o interface
 }
