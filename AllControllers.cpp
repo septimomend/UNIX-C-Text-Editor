ea5df68bb4cb83bbf5a1781568174b1b2de88626
septimomend <chapkailo.ivan@gmail.com>
2017-06-30 11:57:53 +0300

    source file for definition of AllControllers.h

---
+++ AllControllers.cpp
@@ -0,0 +1,21 @@
+// AllControllers.cpp -- source file
+/*
+ *
+ * Author: septimomend (Ivan Chapkailo)
+ *
+ * 30.06.2017
+ *
+ */
+
+#include "stdafx.h"
+#include "AllControllers.h"
+
+using namespace std;
+
+// default csrt
+//
+AllControllers::AllControllers()
+{
+}
+
+// TODO: add functions definitions
