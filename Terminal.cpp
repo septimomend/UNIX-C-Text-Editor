3988fedf40caeb48f36bd8755c30340644dc1291
septimomend <chapkailo.ivan@gmail.com>
2017-06-30 12:13:12 +0300

    TODO: add definitions

---
+++ Terminal.cpp
@@ -0,0 +1,13 @@
+// Terminal.cpp -- source file
+/*
+ *
+ * Author: septimomend (Ivan Chapkailo)
+ *
+ * 30.06.2017
+ *
+ */
+
+#include "stdafx.h"
+#include "Terminal.h"
+
+// TODO: add definitions
