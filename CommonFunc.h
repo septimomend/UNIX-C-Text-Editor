057a8b27f52229e266912d7d85ad57e60b031652
septimomend <chapkailo.ivan@gmail.com>
2017-07-03 16:30:41 +0300

    class contains common functions

---
+++ CommonFunc.h
@@ -0,0 +1,18 @@
+// CommonFunc.h -- header file
+/*
+ *
+ * Author: septimomend (Ivan Chapkailo)
+ *
+ * 30.06.2017
+ *
+ */
+#pragma once
+#include "stdafx.h"
+
+class Common
+{
+public:
+  void statusMsg(const char *fmt, ...);
+  void updateScreen();
+  char *callPrompt(char *prompt, void (*callback)(char *, int));
+};
