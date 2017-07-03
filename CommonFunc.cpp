5f01d1da7c57872ee98696c23f59ea5e28694d68
septimomend <chapkailo.ivan@gmail.com>
2017-07-03 16:33:17 +0300

    TODO: add Common class (CommonFunc.h) definition

---
+++ CommonFunc.cpp
@@ -0,0 +1,13 @@
+// CommonFunc.cpp -- source file
+/*
+ *
+ * Author: septimomend (Ivan Chapkailo)
+ *
+ * 02.07.2017
+ *
+ */
+
+#include "stdafx.h"
+#include "CommonFunc.h"
+
+// TODO: add Common class (CommonFunc.h) definition
