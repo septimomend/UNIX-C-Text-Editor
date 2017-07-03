d64676357cdfd5640a3b58c3eab60a0c55d1d091
septimomend <chapkailo.ivan@gmail.com>
2017-07-03 16:14:44 +0300

    realized encapsulation

--- AllControllers.h
+++ AllControllers.h
@@ -16,8 +16,10 @@ class AllControllers : public ConfigurationController
 {
 public:
   AllControllers(); // cstr
+  ConfigurationController getConfigObj() const;
   // TODO: public data
 
+  ConfigurationController
 private:
   SyntaxController snx[] = {
                             {"c", pFileExtension, pKeywords,
