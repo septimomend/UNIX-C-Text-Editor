57f6bad99f7172dfcb8964b0095c1f64110c0234
septimomend <chapkailo.ivan@gmail.com>
2017-07-06 17:07:55 +0300

    realized escape-sequence

--- Terminal.cpp
+++ Terminal.cpp
@@ -14,6 +14,12 @@ Terminal::Terminal() // cstr
 {
 }
 
+/*
+ * 0 - STDIN_FILENO - standard input descriptor
+ * 1 - STDOUT_FILENO - standard output descriptor
+ * 2 - STDERR_FILENO - standard errors output descriptor
+ */
+
 void Terminal::emergencyDestruction(const char* str)
 {
   write(STDOUT_FILENO, "\x1b[2J", 4); // write clr code to file
@@ -58,7 +64,88 @@ void Terminal::rowModeOn()
 
 int Terminal::whatKey() // defines key pressing
 {
-  // TODO
+  int countRD;
+  char c;
+  while ((countRD = read(STDIN_FILENO, &c, 1)) != 1)  // read 1 byte from input device and write to c
+  {
+    if (countRD == -1 && errno != EAGAIN)
+      emergencyDestruction("read");
+  }
+
+  if (c == '\x1b') // if this is the escape key
+  {
+    char sequence[3];
+
+    if (read(STDIN_FILENO, &sequence[0], 1) != 1) // if no errors
+      return '\x1b';
+    if (read(STDIN_FILENO, &sequence[1], 1) != 1)
+      return '\x1b';
+
+    // this is escape-sequence
+    //          \x1b[...m
+    // where ... is numbers of parameters
+    // more on https://en.wikipedia.org/wiki/Escape_sequences_in_C
+    //
+    if (sequence[0] == '[')
+    {
+      if (sequence[1] >= '0' && sequence[1] <= '9')
+      {
+        if (read(STDIN_FILENO, &sequence[2], 1) != 1)
+          return '\x1b'; // if escape calls 3rd time - exit
+        if (sequence[2] == '~')
+        {
+          switch (sequence[1])
+          {
+            case '1':
+              return HOME_KEY;
+            case '3':
+              return DELETE_KEY;
+            case '4':
+              return END_KEY;
+            case '5':
+              return PAGE_UP;
+            case '6':
+              return PAGE_DOWN;
+            case '7':
+              return HOME_KEY;
+            case '8':
+              return END_KEY;
+          }
+        }
+      }
+      else  // else if sequence[1] is not numbers
+      {
+        switch (sequence[1])
+        {
+          case 'A':
+            return ARROW_UP;
+          case 'B':
+            return ARROW_DOWN;
+          case 'C':
+            return ARROW_RIGHT;
+          case 'D':
+            return ARROW_LEFT;
+          case 'H':
+            return HOME_KEY;
+          case 'F':
+            return END_KEY;
+        }
+      }
+    }
+    else if (sequence[0] == 'O')
+    {
+      switch (sequence[1])
+      {
+        case 'H':
+          return HOME_KEY;
+        case 'F':
+          return END_KEY;
+      }
+    }
+    return '\x1b';
+  }
+  else
+    return c;
 }
 
 int Terminal::getCursorPosition(int *row, int *column) // returns cursor position
