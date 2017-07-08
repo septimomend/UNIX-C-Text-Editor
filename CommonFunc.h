b3013757ba3c6fc76c8d11403aacba1815de8a88
septimomend <chapkailo.ivan@gmail.com>
2017-07-08 13:21:43 +0300

    added new functions

--- CommonFunc.h
+++ CommonFunc.h
@@ -6,14 +6,33 @@
  * 30.06.2017
  *
  */
+
 #pragma once
 #include "stdafx.h"
+#include "AllControllers.h"
+#include "AdditionalBuffer.h"
 
 class Common : public AllControllers
 {
 public:
-  Common(); // cstr
+  Common(AllControllers* all); // cstr
+
+  /*
+   * draws
+   */
+  void drawRows(Adbfr* abfr);
+  void drawStatusBar(Adbfr* abfr);
+  void drawMessageBar(Adbfr* abfr);
+
+  /*
+   * operations
+   */
   void statusMsg(const char *fmt, ...);
   void updateScreen();
   char *callPrompt(char *prompt, void (*callback)(char *, int));
+  void scrolling();
+
+private:
+  ConfigurationController* m_cnfg;
+  Adbfr m_abfr;
 };
