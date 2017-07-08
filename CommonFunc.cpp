74780f7da3aaf71b2c9f902f949f120bb287a9e2
septimomend <chapkailo.ivan@gmail.com>
2017-07-08 12:18:42 +0300

    statusMsg - defined

--- CommonFunc.cpp
+++ CommonFunc.cpp
@@ -10,4 +10,28 @@
 #include "stdafx.h"
 #include "CommonFunc.h"
 
-// TODO: add Common class (CommonFunc.h) definition
+
+Common::Common() // cstr
+{
+}
+
+void Common::statusMsg(const char *fmt, ...)
+{
+  AllControllers all;
+  ConfigurationController cnfg = all.getConfigObj();
+  va_list arg;         // for unknown number of parameters
+  va_start(arg, fmt);  // initialize arg
+  vsnprintf(cnfg.statusMessage, sizeof(cnfg.statusMessage), fmt, arg); // output parameters to statusMessage
+  va_end(arg); // end
+  cnfg.statusMessageTime = time(NULL); // gets time
+}
+
+void Common::updateScreen()
+{
+
+}
+
+char* Common::callPrompt(char *prompt, void (*callback)(char *, int))
+{
+
+}
