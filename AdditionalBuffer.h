345fd2602a5ba517671c4e9930f28b589adf14bb
septimomend <chapkailo.ivan@gmail.com>
2017-07-08 12:42:25 +0300

    + additional intermediary buffer

---
+++ AdditionalBuffer.h
@@ -0,0 +1,20 @@
+// AdditionalBuffer.h -- header file
+/*
+ *
+ * Author: septimomend (Ivan Chapkailo)
+ *
+ * 08.07.2017
+ *
+ */
+
+#pragma once
+
+#include "stdafx.h"
+#include "AllControllers.h"
+
+class Adbfr
+{
+public:
+  char *bfr;
+  int len;
+};
