ce793375b1ab98824d751ef8471de63133200987
septimomend <chapkailo.ivan@gmail.com>
2017-07-13 20:41:27 +0300

    callPrompt func defined

--- Editormend/CommonFunc.cpp
+++ Editormend/CommonFunc.cpp
@@ -76,7 +76,58 @@ void Common::updateScreen()
   m_abfr.freeBfr(); // free memory
 }
 
-char* Common::callPrompt(char *prompt, void (*callback)(char *, int))
+char* Common::callPrompt(char *prompt, void (*callback)(char*, int))
 {
-  // TODO
+  size_t buffsize = 128;
+  char* bfr = malloc(buffsize); // allocate memory
+
+  size_t bufflen = 0;           // lenght
+  bfr[0] = '\0';
+
+  while (1)
+  {
+    statusMsg(prompt, bfr); // set prompt
+    updateScreen();         // and update screen
+
+    int key = tml.whatKey();  // read key
+
+    if (key == DELETE_KEY || key == CTRL_KEY('h') || key == BACKSPACE)
+    {
+      if (bufflen != 0)         // if buffer is not empty
+        bfr[--bufflen] = '\0';  // set end of row
+    }
+    else if (key == '\x1b')     // if this is ESC
+    {
+      statusMsg("");          // erase
+      if (callback)           // if callback is not NULL
+        callback(bfr, key);   // call func for callback which pointed by callback()
+      free(bfr);              // free memory
+      return NULL;            // and cancel
+    }
+    else if (key == '\r')     // if carriage return (move the cursor at the start of the line)
+    {
+      if (bufflen != 0)       // and bufflen is not empty
+      {
+        statusMsg("");        // clean message
+        if (callback)
+          callback(bfr, key); // and get callback
+        return bfr;           // returning of buffer
+      }
+    }
+    else if (!iscntrl(key) && key < 128) // checks, whether the argument, transmitted via the parameter сharacter, control character
+    {
+      /*
+       * reallocation of buffer if needs
+       */
+      if (bufflen == buffsize - 1)
+      {
+        buffsize *= 2;
+        bfr = realloc(bfr, buffsize);
+      }
+      bfr[bufflen++] = key; // write this key to buffer
+      bfr[bufflen] = '\0';  // and end of row
+    }
+
+    if (callback)
+      callback(bfr, key);   // call of callback
 }
