9a149e4fdf3f69e771b55b1f7b2b2c0fc4a21e32
septimomend <chapkailo.ivan@gmail.com>
2017-07-23 12:56:39 +0300

    picSyntaxClr() - func colorize rows in dependency with filename

--- Editormend/src/AllControllers.cpp
+++ Editormend/src/AllControllers.cpp
@@ -25,5 +25,35 @@ ConfigurationController* AllControllers::getConfigObj() const
 
 void AllControllers::pickSyntaxClr()
 {
-  // TODO
+  configObj.pSyntaxObj = NULL;
+  if (configObj.pFilename == NULL)
+    return;
+
+  for (unsigned int j = 0; j < SNX_SZ; j++)
+  {
+    SyntaxController *sctrl = &snx[j];              // initialize syntax instance
+    unsigned int i = 0;
+    while (sctrl->ppFileAccordance[i])
+    {
+      char *ac = strstr(configObj.pFilename, sctrl->ppFileAccordance[i]);  // char that accordance with pFilename
+      if (ac != NULL)                                                      // if that char is accordanced with filename
+      {
+        int acsz = strlen(sctrl->ppFileAccordance[i]);
+        if (sctrl->ppFileAccordance[i][0] != '.' || ac[acsz] == '\0')      // if met point of file expansion, i.e. filename.<type> or end of filename without expansion
+        {
+          configObj.pSyntaxObj = sctrl;                                    // copy data to main syntax object
+
+          int pastRows;
+          // colorize rows before knowing of filename and expansion
+          //
+          for (pastRows = 0; pastRows < configObj.rowCount; pastRows++)
+          {
+            updateSyntax(&configObj.pRowObj[pastRows]);
+          }
+          return;
+        }
+      }
+      i++;
+    }
+  }
 }
